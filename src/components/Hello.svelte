e526c15701ce7ab1d116bc8c129083b53e63fabf
uemuragame5683 <nobuyoshi5683@gmail.com>
2021-08-01 01:53:26 +0900

    ページを追加

---
+++ src/components/Hello.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { push } from 'svelte-spa-router'
+</script>
+
+<!-- HTML部 -->
+<div class="container hello">
+	<h2 class="h3 mb-3">HELLO</h2>
+	<div on:click={() => push('/')}>TOP</div>
+</div>
+
+<style>
+	.container.hello {
+		margin-bottom: 20px;
+	}
+</style>
---
+++ src/components/Home.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { push } from 'svelte-spa-router'
+</script>
+
+<div class="container hello">
+	<h2 class="h3 mb-3">SVELTE</h2>
+	<div on:click={() => push('/hello')}>Hello</div>
+	<div on:click={() => push('/task')}>Task</div>
+</div>
+
+<style>
+	.container.hello {
+		margin-bottom: 20px;
+	}
+</style>
