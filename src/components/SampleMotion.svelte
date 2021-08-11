fd0c33745804efe84e6f67a708bf9d5d944452ef
uemuragame5683 <nobuyoshi5683@gmail.com>
2021-08-12 00:29:41 +0900

    ファイル追加

---
+++ src/components/SampleMotion.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { push } from 'svelte-spa-router'
+	
+</script>
+
+<!-- HTML部 -->
+<div class="container">
+	<h2 class="h3 mb-3 title">Sample Motion</h2>
+	<div class="samplelist">
+		<div class="sampleli">
+			<p class="sub-title"></p>
+
+		</div>
+	</div>
+	<div class="pointer" on:click={() => push('/')}>TOP</div>
+</div>
---
+++ src/components/SampleStore.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { push } from 'svelte-spa-router'
+	
+</script>
+
+<!-- HTML部 -->
+<div class="container">
+	<h2 class="h3 mb-3 title">Sample Store</h2>
+	<div class="samplelist">
+		<div class="sampleli">
+			<p class="sub-title"></p>
+		</div>
+	</div>
+	<div class="pointer" on:click={() => push('/')}>TOP</div>
+</div>
